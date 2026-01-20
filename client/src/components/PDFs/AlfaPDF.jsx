e092370db8e6adef19ccd77c24c495850cd249a9
gcoronar-prog <gcoronarc4621@gmail.com>
2026-01-20 12:42:31 -0300

    wip pdf alfa 20.01

--- client/src/components/PDFs/AlfaPDF.jsx
+++ client/src/components/PDFs/AlfaPDF.jsx
@@ -8,7 +8,7 @@ const AlfaPDF = forwardRef(({ data }, ref) => {
     Object.entries(data.tipo_afectados || {}).map(([k, v]) => [
       k.toUpperCase(),
       v,
-    ])
+    ]),
   );
 
   const eventoIzq = [
@@ -52,23 +52,27 @@ const AlfaPDF = forwardRef(({ data }, ref) => {
   return (
     <div ref={ref} className="alfa-pdf">
       {/* ================= HEADER ================= */}
-      <div className="text-center fw-bold mb-2">
-        INFORME DE INCIDENTE O EMERGENCIA Nº {data.cod_alfa}
+      <div className="text-center mb-2">
+        <span className="fw-bold" style={{ fontSize: "15px" }}>
+          INFORME DE INCIDENTE O EMERGENCIA Nº {data.cod_alfa}
+        </span>
       </div>
 
       {/* ================= 1. IDENTIFICACIÓN ================= */}
-      <section className="border p-2 mb-2">
-        <div className="fw-bold mb-1">1. IDENTIFICACIÓN</div>
-
-        <div className="row">
-          <div className="col">REGIÓN: {data.region}</div>
-          <div className="col">PROVINCIA: {data.provincia}</div>
-          <div className="col">COMUNA: {data.comuna}</div>
-        </div>
-
-        <div className="row mt-1">
-          <div className="col">FUENTE: {data.fuente_info}</div>
-          <div className="col">FONO: {data.telefono}</div>
+      <section className="mb-2" style={{ border: "1px solid" }}>
+        <div className="d-flex mt-2 p-2">
+          <div className="flex-shrink-2 mb-1 me-1 ms-1 p-0">
+            <span className="fw-bold">1. IDENTIFICACIÓN:</span>
+          </div>
+          <div className="flex-shrink-2 ms-3">
+            <span>REGIÓN: {data.region}</span>
+            <div className="flex-fill mt-1">FUENTE(S): {data.fuente_info}</div>
+          </div>
+          <div className="flex-shrink-2 ms-4">PROVINCIA: {data.provincia}</div>
+          <div className="flex-shrink-2 ms-4">
+            COMUNA: {data.comuna}
+            <div className="flex-fill mt-1">FONO: {data.telefono}</div>
+          </div>
         </div>
       </section>
 
@@ -104,6 +108,7 @@ const AlfaPDF = forwardRef(({ data }, ref) => {
                           ? "solid"
                           : "none",
                         borderWidth: "0.5px",
+                        padding: "0.8px",
                       }}
                     >
                       <label
@@ -131,8 +136,11 @@ const AlfaPDF = forwardRef(({ data }, ref) => {
                         return (
                           <tr key={eveIzq}>
                             <td
-                              className="border"
-                              style={{ width: "10%", height: "80%" }}
+                              style={{
+                                border: "1px solid",
+                                width: "10%",
+                                height: "80%",
+                              }}
                             >
                               <span className="ms-2">
                                 {data.tipo_evento.includes(eveIzq) ? "X" : ""}
@@ -143,7 +151,9 @@ const AlfaPDF = forwardRef(({ data }, ref) => {
                             </td>
                             {eveDer ? (
                               <>
-                                <td className="border" style={{ width: "10%" }}>
+                                <td
+                                  style={{ border: "1px solid", width: "10%" }}
+                                >
                                   <span className="ms-2">
                                     {data.tipo_evento.includes(eveDer)
                                       ? "X"
@@ -388,25 +398,25 @@ const AlfaPDF = forwardRef(({ data }, ref) => {
         <div className="row g-2 mt-0">
           <div className="col mt-0 p-0">
             <div className="mt-1 p-0">
-              <span className="fw-bold p-0 ms-2">4. DECISIONES</span>
+              <span className="fw-bold p-0 ms-2 mb-2">4. DECISIONES</span>
               <span className="p-1">
                 &nbsp; ACCIONES Y SOLUCIONES INMEDIATAS:&nbsp;&nbsp; &nbsp;
               </span>
-              <div className="ms-3 me-4 mt-0">
+              <div className="ms-3 me-4 mt-2">
                 <div className="line-box mt-0 align-bottom">
                   <span>{data.acciones}</span>
                 </div>
-                <div className="line-box mt-0"></div>
-                <div className="line-box mt-0"></div>
+                <div className="line-box mt-1"></div>
+                <div className="line-box mt-1 mb-1"></div>
               </div>
             </div>
           </div>
           <div className="col-4 m-0 p-1">
-            <div className="fw-bold ms-1 me-5">
+            <div className="fw-bold ms-1 me-5 mb-1">
               OPORTUNIDAD (TPO) RESTABLECIMIENTO:
             </div>
-            <div className="line-box p-0">
-              <span>{data.oportunidad}</span>
+            <div className="line-box p-0 mb-1 me-3">
+              <span style={{ fontSize: "12px" }}>{data.oportunidad}</span>
             </div>
             <div className="line-box"></div>
           </div>
@@ -418,25 +428,25 @@ const AlfaPDF = forwardRef(({ data }, ref) => {
         className="p-0 m-0"
         style={{ border: "1px solid", padding: "0px 0px" }}
       >
-        <div className="mt-1 p-0">
+        <div className="mt-1 p-0 mb-1">
           <span className="fw-bold ms-1 mt-0 p-0">
             5. RECURSOS INVOLUCRADOS
           </span>
           &nbsp;&nbsp; <span>TIPO (HUMANO-MATERIAL)</span>
         </div>
-        <div className="line-box me-4 ms-1">
-          <span>{data.recursos}</span>
+        <div className="line-box me-4 ms-1 mb-1">
+          <span style={{ fontSize: "12px" }}>{data.recursos}</span>
         </div>
-        <div className="line-box me-4 ms-1"></div>
-        <div className="line-box me-4 ms-1"></div>
+        <div className="line-box me-4 ms-1 mb-1"></div>
+        <div className="line-box me-4 ms-1 mb-1"></div>
       </section>
 
       {/* ================= 6. EVALUACIÓN ================= */}
-      <section className="border">
+      <section style={{ border: "1px solid", padding: "0px 0px" }}>
         <div className="row">
           <div className="col">
             <div className="fw-bold ms-2">6. EVALUACIÓN DE NECESIDADES</div>
-            <div className="ms-1">
+            <div className="ms-1" style={{ marginBottom: "60%" }}>
               <table className="m-0 ">
                 <thead></thead>
                 <tbody className="px-0 ps-1">
@@ -446,7 +456,14 @@ const AlfaPDF = forwardRef(({ data }, ref) => {
                   ].map((tipo) => {
                     return (
                       <tr key={tipo}>
-                        <td className="border">
+                        <td
+                          style={{
+                            border: "1px solid",
+                            padding: "0px 0px",
+                            width: "5%",
+                          }}
+                          className="text-center"
+                        >
                           {data.necesidades.includes("") ? "X" : ""}
                         </td>
                         <td className="ms-5">{tipo}</td>
@@ -455,75 +472,92 @@ const AlfaPDF = forwardRef(({ data }, ref) => {
                   })}
                 </tbody>
               </table>
+              <div className="line-box mt-1"></div>
             </div>
           </div>
           <div className="col">
             {/* ================= 7. CAPACIDAD ================= */}
 
-            <div className="fw-bold mb-1">7. NIVELES DE EMERGENCIA</div>
-            <table className="m-0">
-              <thead></thead>
-              <tbody className="m-4">
-                {/*  {arrayEmergencia.map((emer) => {
+            <div className="fw-bold mb-2 ms-1">7. NIVELES DE EMERGENCIA</div>
+            <div className="mb-2">
+              <table className="m-0 ms-1 p-1 me-2">
+                <thead></thead>
+                <tbody className="m-4">
+                  {arrayEmergencia.map((e, i) => {
                     return (
-                      <tr key={emer}>
-                        <td className="border"></td>
-                        <td>{emer}</td>
+                      <tr className="">
+                        <td
+                          style={{
+                            border: "1px solid",
+                            width: "6%",
+                            height: "5%",
+                          }}
+                        ></td>
+                        <td className="m-0 mt-0 pt-0" style={{ width: "90%" }}>
+                          <span
+                            className="fw-bold p-0 m-0"
+                            style={{
+                              fontSize: "10px",
+                              letterSpacing: "-1px",
+                              lineHeight: "-1px",
+                            }}
+                          >
+                            {e}
+                          </span>
+                          <span
+                            style={{
+                              fontSize: "10px",
+                              letterSpacing: "-1px",
+                              lineHeight: "-1px",
+                            }}
+                          >
+                            {detalleEmergencia[i]}
+                          </span>
+                        </td>
                       </tr>
                     );
                   })}
-                  {detalleEmergencia.map((emerg) => {
-                    return (
-                      <tr key={emerg}>
-                        <td className="border"></td>
-                        <td>{emerg}</td>
-                      </tr>
-                    );
-                  })}*/}
-                {arrayEmergencia.map((e, i) => {
-                  return (
-                    <tr className="">
-                      <td className="border" style={{ width: "5%" }}></td>
-                      <td className="m-0 mt-0 pt-0" style={{ width: "90%" }}>
-                        <span
-                          className="fw-bold p-0 m-0"
-                          style={{
-                            fontSize: "8px",
-                            letterSpacing: "-1px",
-                            lineHeight: "-1px",
-                          }}
-                        >
-                          {e}
-                        </span>
-                        <span
-                          style={{
-                            fontSize: "7px",
-                            letterSpacing: "-1px",
-                            lineHeight: "-1px",
-                          }}
-                        >
-                          {detalleEmergencia[i]}
-                        </span>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+                </tbody>
+              </table>
+            </div>
 
             {/* ================= 8. OBSERVACIONES ================= */}
-
-            <div className="fw-bold">8. OBSERVACIONES</div>
-            <div className="line-box">{data.observaciones}</div>
+            <div className="mt-1">
+              <div className="fw-bold ms-1 mb-2">8. OBSERVACIONES</div>
+              <div className="line-box ms-2 me-3">
+                <span style={{ fontSize: "10px" }}>{data.observaciones}</span>
+              </div>
+            </div>
           </div>
         </div>
       </section>
       {/* ================= 9. RESPONSABLE ================= */}
-      <section className="border p-2">
-        <div className="fw-bold">9. RESPONSABLE DEL INFORME</div>
-        <span>IDENTIFICACIÓN: {data.funcionario}</span>
-        <span>FECHA: {data.fecha_documento}</span>
-        <span>{horaDocumento}</span>
+      <section
+        style={{
+          border: "1px solid",
+          padding: "0px 0px",
+        }}
+      >
+        <div className="fw-bold ms-1 mb-2" style={{ marginTop: "0.5px" }}>
+          9. RESPONSABLE DEL INFORME
+        </div>
+
+        <div className="ms-2 mb-1 d-flex">
+          <div className="flex-fill">
+            IDENTIFICACIÓN:
+            <span className="ms-2">
+              <u>{data.funcionario}</u>
+            </span>
+          </div>
+          <div className="flex-fill">
+            <span>FECHA: </span>
+            <span className="ms-2">{data.fecha_documento}</span>
+          </div>
+          <div className="flex-fill">
+            <span>HORA:</span>
+            <span className="ms-2 line-box"></span>
+          </div>
+        </div>
       </section>
     </div>
   );
