bd77e544c685afa9bb250dd00402adbe01c18d72
gcoronar-prog <gcoronarc4621@gmail.com>
2026-02-24 17:04:40 -0300

    wip inventario grd

--- server/src/controllers/inventarioGRD.controller.js
+++ server/src/controllers/inventarioGRD.controller.js
@@ -56,7 +56,7 @@ const getListaProductos = async (req, res) => {
     if (rows.length === 0) {
       console.error("No existen registros");
     }
-    return res.json(rows);
+    return res.status(201).json(rows);
   } catch (error) {
     console.error(error);
     return res
@@ -66,277 +66,378 @@ const getListaProductos = async (req, res) => {
 };
 
 const createProducto = async (req, res) => {
-  const client = await pool.connect();
   const data = req.body;
   try {
-    await client.query("BEGIN");
+    const { rows: result } = await pool.query(nuevo_producto, [
+      data.nombre_producto,
+      data.observ_produ,
+      data.tipo_produ,
+      data.cantidad,
+      data.precio_unit,
+      data.precio_total,
+      data.ubicacion,
+    ]);
 
-    const inventario = await client.query(
-      "INSERT INTO inventario_grd \
-                (ubicacion,observaciones,usuario_creador,fecha_creado,prestamo,usuario_prestamo)\
-                VALUES ($1,$2,$3,$4,$5,$6) RETURNING *",
-      [
-        data.ubicacion,
-        data.observaciones,
-        data.usuario_creador,
-        data.fecha_creado,
-        data.prestamo,
-        data.usuario_prestamo,
-      ],
-    );
-    const idProducto = inventario.rows[0].cod_producto;
-
-    const producto = await client.query(
-      "INSERT INTO productos_grd \
-          (marca,modelo,serial,desc_producto,unidad_medida,existencias,cod_producto,precio_unitario)\
-           VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *",
-      [
-        data.marca,
-        data.modelo,
-        data.serial,
-        data.desc_producto,
-        data.unidad_medida,
-        data.existencias,
-        idProducto,
-        data.precio_unitario,
-      ],
-    );
-
-    await client.query("COMMIT");
-
-    return res.json({
-      registro_producto: producto.rows,
-      registro_inventario: inventario.rows,
-    });
+    return res.status(201).json(result);
   } catch (error) {
-    await client.query("ROLLBACK");
     console.error(error);
     return res
       .status(500)
       .json({ message: "Problemas de conexion con servidor" });
-  } finally {
-    client.release();
   }
 };
 
 const updateInventario = async (req, res) => {
-  const client = await pool.connect();
+  const { id } = req.params;
+  const data = req.body;
   try {
-    const { id } = req.params;
-    const data = req.body;
-    await client.query("BEGIN");
-
-    const inven = await client.query(
-      "UPDATE inventario_grd SET\
-                ubicacion=$1,observaciones=$2, id_productos_grd=$3, prestamo=$4,usuario_prestamo=$5\
-                WHERE id_producto = $6 RETURNING *",
-      [
-        data.ubicacion,
-        data.observaciones,
-        data.id_productos_grd,
-        data.prestamo,
-        data.usuario_prestamo,
-        id,
-      ],
-    );
-    const idProductGRD = inven.rows[0].cod_producto;
-
-    const produc = await client.query(
-      "UPDATE productos_grd SET\
-        marca=$1,modelo=$2,serial=$3,desc_producto=$4,unidad_medida=$5,existencias=$6,precio_unitario=$7,precio_total=$8\
-        WHERE cod_producto = $9  RETURNING *",
-      [
-        data.marca,
-        data.modelo,
-        data.serial,
-        data.desc_producto,
-        data.unidad_medida,
-        data.existencias,
-        data.precio_unitario,
-        data.precio_total,
-        idProductGRD,
-      ],
-    );
-    await client.query("COMMIT");
-
-    return res.json({
-      inventario: inven.rows,
-      productos: produc.rows,
-    });
+    const { rows: result } = await pool.query(update_producto, [
+      data.nombre_producto,
+      data.observ_produ,
+      data.tipo_produ,
+      data.cantidad,
+      data.precio_unit,
+      data.precio_total,
+      data.ubicacion,
+      id,
+    ]);
+    return res.status(201).json(result);
   } catch (error) {
-    await client.query("ROLLBACK");
     console.error(error);
     return res
       .status(500)
       .json({ message: "Problemas de conexión con el servidor" });
-  } finally {
-    client.release();
   }
 };
 
 const deleteInventario = async (req, res) => {
-  const client = await pool.connect();
+  const { id } = req.params;
   try {
-    const { id } = req.params;
-    await client.query("BEGIN");
-    const inventario = await client.query(
-      "DELETE FROM inventario_grd WHERE id_producto=$1 RETURNING *",
-      [id],
-    );
-    const idProductGRD = inventario.rows[0].cod_producto;
-    const producto = await client.query(
-      "DELETE FROM productos_grd WHERE cod_producto=$1 RETURNING *",
-      [idProductGRD],
-    );
-    await client.query("COMMIT");
-
-    return res.json({
-      inventarioDelete: inventario.rows,
-      productoDelete: producto.rows,
-    });
-  } catch (error) {
-    await client.query("ROLLBACK");
-    console.error(error);
-    return res
-      .status(500)
-      .json({ message: "Problemas de conexión con el servidor" });
-  } finally {
-    client.release();
-  }
-};
-
-const getLastInventario = async (req, res) => {
-  const client = await pool.connect();
-  try {
-    await client.query("BEGIN");
-    const inventario = await client.query(
-      "SELECT * FROM inventario_grd ORDER BY id_producto DESC LIMIT 1",
-    );
-    const producto = await client.query(
-      "SELECT * FROM prestamo_grd ORDER BY id_prestamo DESC LIMIT 1",
-    );
-    await client.query("COMMIT");
-
-    return res.json({
-      ultimo: inventario.rows,
-      ultimo_producto: producto.rows,
-    });
-  } catch (error) {
-    await client.query("ROLLBACK");
-    console.error(error);
-    return res.json({ message: "Problemas de conexion con el servidor" });
-  } finally {
-    client.release();
-  }
-};
-
-const getFirstInventario = async (req, res) => {
-  const client = await pool.connect();
-  try {
-    await client.query("BEGIN");
-    const inventario = await client.query(
-      "SELECT * FROM inventario_grd ORDER BY id_producto ASC LIMIT 1",
-    );
-    const producto = await client.query(
-      "SELECT * FROM productos_grd ORDER BY id_productos_grd ASC LIMIT 1",
-    );
-    await client.query("COMMIT");
-
-    return res.json({
-      primero: inventario.rows,
-      primer_producto: producto.rows,
-    });
-  } catch (error) {
-    await client.query("ROLLBACK");
-    console.error(error);
-    return res.json({ message: "Problemas de conexion con el servidor" });
-  } finally {
-    client.release();
-  }
-};
-
-const getPrevInventario = async (req, res) => {
-  const client = await pool.connect();
-  try {
-    const { id } = req.params;
-    await client.query("BEGIN");
-    const prevInventario = await client.query(
-      "SELECT * FROM inventario_grd WHERE id_producto < \
-        (SELECT id_producto FROM inventario_grd WHERE id_producto=$1) ORDER BY id_producto DESC LIMIT 1",
-      [id],
-    );
-    const codProducto = prevInventario.rows[0].cod_producto;
-
-    const prevProducto = await client.query(
-      "SELECT * FROM productos_grd WHERE id_productos_grd <\
-        (SELECT id_productos_grd FROM productos_grd WHERE cod_producto=$1)\
-	    ORDER BY id_productos_grd DESC LIMIT 1",
-      [codProducto],
-    );
-
-    if (prevInventario.length === 0 && prevProducto === 0) {
-      return res
-        .status(404)
-        .json({ message: "No se encuentran registros anteriores" });
+    const { rows: result } = await pool.query(delete_producto, [id]);
+    if (result.length === 0) {
+      return res.status(404).json({ message: "No se encuentra el producto" });
     }
-    await client.query("COMMIT");
-    return res.json({
-      inventario_prev: prevInventario.rows,
-      productos_prev: prevProducto.rows,
-    });
+    return res.status(201).json(result);
   } catch (error) {
-    await client.query("ROLLBACK");
     console.error(error);
     return res
       .status(500)
       .json({ message: "Problemas de conexión con el servidor" });
-  } finally {
-    client.release();
   }
 };
 
-const getNextInventario = async (req, res) => {
-  const client = await pool.connect();
+const getListEntradas = async (req, res) => {
   try {
-    const { id } = req.params;
-    await client.query("BEGIN");
-    const nextInventario = await client.query(
-      "SELECT * FROM inventario_grd WHERE id_producto > \
-          (SELECT id_producto FROM inventario_grd WHERE id_producto=$1) ORDER BY id_producto ASC LIMIT 1",
+    const { rows } = await pool.query("SELECT * FROM inventario_grd");
+    if (rows.length === 0) {
+      console.error("No existen registros");
+    }
+    return res.status(201).json(rows);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Problemas de conexión con el servidor" });
+  }
+};
+
+const getEntrada = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { rows } = await pool.query(
+      "SELECT * FROM inventario_grd WHERE id_producto=$1",
       [id],
     );
-    const codProducto = nextInventario.rows[0].cod_producto;
-    console.log(codProducto);
-    const nextProducto = await client.query(
-      "SELECT * FROM productos_grd WHERE id_productos_grd >\
-      (SELECT id_productos_grd FROM productos_grd WHERE cod_producto=$1)\
-	    ORDER BY id_productos_grd asc LIMIT 1",
-      [codProducto],
-    );
-
-    if (nextInventario.length === 0 || nextProducto.length === 0) {
-      return res.status(404).json({ message: "No se encuentran registros" });
+    if (rows.length === 0) {
+      console.error("No existen registros");
     }
-    await client.query("COMMIT");
-    return res.json({
-      inventario_next: nextInventario.rows,
-      productos_next: nextProducto.rows,
-    });
+    return res.status(201).json(rows);
   } catch (error) {
-    await client.query("ROLLBACK");
     console.error(error);
     return res
       .status(500)
       .json({ message: "Problemas de conexión con el servidor" });
-  } finally {
-    client.release();
   }
 };
 
+const createEntrada = async (req, res) => {
+  const data = req.body;
+  try {
+    const { rows: result } = await pool.query(entrada_grd, [
+      data.ubicacion,
+      data.observaciones,
+      data.usuario_creador,
+      data.fecha_creado,
+      data.marca,
+      data.modelo,
+      data.cantidad,
+      data.tipo_producto,
+      data.factura,
+      data.orden_compra,
+      data.proveedor,
+      data.id_producto,
+    ]);
+
+    return res.status(201).json(result);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Problemas de conexion con servidor" });
+  }
+};
+
+const updateEntrada = async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  try {
+    const { rows: result } = await pool.query(update_entrada, [
+      data.ubicacion,
+      data.observaciones,
+      data.usuario_creador,
+      data.fecha_creado,
+      data.marca,
+      data.modelo,
+      data.cantidad,
+      data.tipo_producto,
+      data.factura,
+      data.orden_compra,
+      data.proveedor,
+      data.producto,
+      id,
+    ]);
+    if (result.length === 0) {
+      return res.status(404).json({ message: "No se encuentra el producto" });
+    }
+    return res.status(201).json(result);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Problemas de conexion con servidor" });
+  }
+};
+
+const deleteEntrada = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { rows } = await pool.query(delete_entrada, [id]);
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "No se encuentra el producto" });
+    }
+    return res.status(201).json(rows);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Problemas de conexion con servidor" });
+  }
+};
+
+// PRESTAMOS
+const getListPrestamos = async (req, res) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM prestamo_grd");
+    if (rows.length === 0) {
+      console.error("No existen registros");
+    }
+    return res.status(201).json(rows);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Problemas de conexión con el servidor" });
+  }
+};
+
+const getPrestamo = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { rows: result } = await pool.query(
+      "SELECT * FROM prestamo_grd WHERE id_prestamo=$1",
+      [id],
+    );
+    if (result.length === 0) {
+      console.error("No existen registros");
+    }
+    return res.status(201).json(result);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Problemas de conexión con el servidor" });
+  }
+};
+
+const createPrestamo = async (req, res) => {
+  const data = req.body;
+  try {
+    const { rows: result } = await pool.query(nuevo_prestamo, [
+      data.nombre,
+      data.marca,
+      data.modelo,
+      data.serial,
+      data.cantidad,
+      data.user_prestamo,
+    ]);
+    return res.status(201).json(result);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Problemas de conexión con el servidor" });
+  }
+};
+
+const updatePrestamo = async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  try {
+    const { rows: result } = await pool.query(update_prestamo, [
+      data.nombre,
+      data.marca,
+      data.modelo,
+      data.serial,
+      data.cantidad,
+      data.user_prestamo,
+      id,
+    ]);
+    if (result.length === 0) {
+      return res.status(404).json({ message: "No se encuentra el producto" });
+    }
+    return res.status(201).json(result);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Problemas de conexión con el servidor" });
+  }
+};
+
+const deletePrestamo = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { rows } = await pool.query(delete_prestamo, [id]);
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "No se encuentra el producto" });
+    }
+    return res.status(201).json(rows);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Problemas de conexión con el servidor" });
+  }
+};
+
+//SALIDAS INVENTARIO
+
+const getListSalidas = async (req, res) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM salida_inventario_grd");
+    if (rows.length === 0) {
+      console.error("No existen registros");
+    }
+    return res.status(201).json(rows);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Problemas de conexión con el servidor" });
+  }
+};
+
+const getSalida = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { rows } = await pool.query(
+      "SELECT * FROM salida_inventario_grd WHERE id_salida=$1",
+      [id],
+    );
+    if (rows.length === 0) {
+      console.error("No existen registros");
+    }
+    return res.status(201).json(rows);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Problemas de conexión con el servidor" });
+  }
+};
+
+const createSalida = async (req, res) => {
+  const data = req.body;
+  try {
+    const { rows: result } = await pool.query(nueva_salida, [
+      data.fecha_salida,
+      data.estado_salida,
+      data.direccion_salida,
+      data.sector_salida,
+      data.tipo_ubi_salida,
+      data.descripcion_salida,
+      data.tipo_evento,
+      data.responsable_salida,
+      data.user_form,
+      data.observaciones,
+    ]);
+    return res.status(201).json(result);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Problemas de conexión con el servidor" });
+  }
+};
+
+const updateSalida = async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  try {
+    const { rows: result } = await pool.query(update_salida, [
+      data.fecha_salida,
+      data.estado_salida,
+      data.direccion_salida,
+      data.sector_salida,
+      data.tipo_ubi_salida,
+      data.descripcion_salida,
+      data.tipo_evento,
+      data.responsable_salida,
+      data.user_form,
+      data.observaciones,
+      id,
+    ]);
+    if (result.length === 0) {
+      return res.status(404).json({ message: "No se encuentra el producto" });
+    }
+    return res.status(201).json(result);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Problemas de conexión con el servidor" });
+  }
+};
+
+const deleteSalida = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { rows } = await pool.query(delete_salida, [id]);
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "No se encuentra el producto" });
+    }
+    return res.status(201).json(rows);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Problemas de conexión con el servidor" });
+  }
+};
 const entrada_grd = `
   INSERT INTO inventario_grd (ubicacion,observaciones,usuario_creador,fecha_creado,\
-  marca,modelo,cantidad,tipo_producto,factura,orden_compra,proveedor,producto,\
-  precio_unitario,precio_total) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14) RETURNING *;
+  marca,modelo,cantidad,tipo_producto,factura,orden_compra,proveedor,id_producto)\
+   VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12) RETURNING *
+
 `;
 
 const nuevo_producto = `
@@ -352,12 +453,12 @@ VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) RETURNING *;`;
 const update_entrada = `
   UPDATE inventario_grd SET ubicacion=$1,observaciones=$2,usuario_creador=$3,fecha_creado=$4,\
   marca=$5,modelo=$6,cantidad=$7,tipo_producto=$8,factura=$9,orden_compra=$10,proveedor=$11,producto=$12,\
-  precio_unitario=$13,precio_total=$14 WHERE id_producto = $15 RETURNING *;
+   WHERE id_inventario = $13 RETURNING *;
 `;
 
 const update_producto = `
   UPDATE productos_grd SET nombre_producto=$1, observ_produ=$2, tipo_produ=$3,\
-  cantidad=$4, precio_unit=$5, precio_total=$6, ubicacion=$7 WHERE id_productos_grd = $8 RETURNING *;
+  cantidad=$4, precio_unit=$5, precio_total=$6, ubicacion=$7 WHERE id_producto = $8 RETURNING *;
 `;
 
 const update_prestamo = `
@@ -371,9 +472,9 @@ const update_salida = `
   user_form=$9,observaciones=$10 WHERE id_salida = $11 RETURNING *;
 `;
 
-const delete_entrada = `DELETE FROM inventario_grd WHERE id_producto=$1 RETURNING *;`;
+const delete_entrada = `DELETE FROM inventario_grd WHERE id_inventario=$1 RETURNING *;`;
 
-const delete_producto = `DELETE FROM productos_grd WHERE id_productos_grd=$1 RETURNING *;`;
+const delete_producto = `DELETE FROM productos_grd WHERE id_producto=$1 RETURNING *;`;
 
 const delete_prestamo = `DELETE FROM prestamo_grd WHERE id_prestamo=$1 RETURNING *;`;
 
@@ -386,9 +487,20 @@ export {
   createProducto,
   updateInventario,
   deleteInventario,
-  getLastInventario,
-  getFirstInventario,
-  getPrevInventario,
-  getNextInventario,
   getListaProductos,
+  getListEntradas,
+  getEntrada,
+  createEntrada,
+  updateEntrada,
+  deleteEntrada,
+  getListPrestamos,
+  getPrestamo,
+  createPrestamo,
+  updatePrestamo,
+  deletePrestamo,
+  getListSalidas,
+  getSalida,
+  createSalida,
+  updateSalida,
+  deleteSalida,
 };
--- server/src/routes/inventarioGRD.routes.js
+++ server/src/routes/inventarioGRD.routes.js
@@ -1,15 +1,26 @@
 import { Router } from "express";
 import {
+  createEntrada,
+  createPrestamo,
   createProducto,
+  createSalida,
+  deleteEntrada,
   deleteInventario,
-  getFirstInventario,
+  deletePrestamo,
+  deleteSalida,
+  getEntrada,
   getInventarioGRD,
   getInventarios,
-  getLastInventario,
   getListaProductos,
-  getNextInventario,
-  getPrevInventario,
+  getListEntradas,
+  getListPrestamos,
+  getListSalidas,
+  getPrestamo,
+  getSalida,
+  updateEntrada,
   updateInventario,
+  updatePrestamo,
+  updateSalida,
 } from "../controllers/inventarioGRD.controller.js";
 
 const router = Router();
@@ -18,12 +29,33 @@ router.get("/inventario/grd", getInventarios);
 router.get("/inventario/grd/:id", getInventarioGRD);
 router.get("/inventario/listado", getListaProductos);
 router.post("/inventario/new", createProducto);
-router.put("/inventario/grd/:id/edit", updateInventario);
-router.delete("/inventario/grd/:id/delete", deleteInventario);
+router.put("/inventario/grd/edit/:id", updateInventario);
+router.delete("/inventario/grd/delete/:id", deleteInventario);
 
-router.get("/inventario/last/grd", getLastInventario);
+//entrada de productos
+router.get("/inventario/entrada/:id", getEntrada);
+router.get("/inventario/entrada/list", getListEntradas);
+router.post("/inventario/entrada/new", createEntrada);
+router.put("/inventario/entrada/edit/:id", updateEntrada);
+router.delete("/inventario/entrada/delete/:id", deleteEntrada);
+
+//prestamos de productos
+router.get("/inventario/prestamo/list", getListPrestamos);
+router.get("/inventario/prestamo/:id", getPrestamo);
+router.post("/inventario/prestamo/new", createPrestamo);
+router.put("/inventario/prestamo/edit/:id", updatePrestamo);
+router.delete("/inventario/prestamo/delete/:id", deletePrestamo);
+
+//salidas de productos
+router.get("/inventario/salida/list", getListSalidas);
+router.get("/inventario/salida/:id", getSalida);
+router.post("/inventario/salida/new", createSalida);
+router.put("/inventario/salida/edit/:id", updateSalida);
+router.delete("/inventario/salida/delete/:id", deleteSalida);
+
+/*router.get("/inventario/last/grd", getLastInventario);
 router.get("/inventario/first/grd", getFirstInventario);
 router.get("/inventario/prev/grd/:id", getPrevInventario);
-router.get("/inventario/next/grd/:id", getNextInventario);
+router.get("/inventario/next/grd/:id", getNextInventario);*/
 
 export default router;
